205da7c45426adc2f00a4f2fcc14ac81bdbb62ff
Darwin Dela Cruz <darwindelacruz0012@gmail.com>
2023-11-09 21:19:00 +0800

    Combine views, URLs, and templates for streamlined functionality

---
+++ ihomp_borrowme_app/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.CombinedListView.as_view(), name='home'),
+
+]
--- ihomp_borrowme_app/views.py
+++ ihomp_borrowme_app/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.views import generic
+from .models import Peripheral, Category, Department
 
-# Create your views here.
+
+class CombinedListView(generic.ListView):
+    model = Category
+    # queryset = Category.objects.order_by("-date_created")
+    template_name = "index.html"
+    # context_object_name = 'category_list'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['department_list'] = Department.objects.all()
+        context['category_list'] = Category.objects.all()
+        context['peripheral_list'] = Peripheral.objects.all()
+        return context
