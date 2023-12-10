bc904e3657ccc8ebe19ddbd1211c687221d5c647
Darwin Dela Cruz <darwindelacruz0012@gmail.com>
2023-12-10 14:42:53 +0800

    Change the RDBMS to PostgreSQL from SQLite3

--- ihomp_borrowme_project/settings.py
+++ ihomp_borrowme_project/settings.py
@@ -76,10 +76,15 @@ WSGI_APPLICATION = 'ihomp_borrowme_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ihomp_borrowme',
+        'USER': 'postgres',
+        'PASSWORD': 'abracadabr@',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
